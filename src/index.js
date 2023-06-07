d48da055bfb45636ef5d27031149baf9b33f8dac
Himanshu <himanshubarde2001@gmail.com>
2023-06-07 23:26:40 +0530

    Reverted last commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cron = require("node-cron");
 
 const { PORT } = require("./config/serverConfig");
 const { sendBasicEmail } = require("./services/email-service");
@@ -12,12 +13,15 @@ const setupAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 
-    sendBasicEmail(
-      "airlineservice@gmail.com",
-      "airlineservice@gmail.com",
-      "This is a tetsing email",
-      "Hey, how are you?"
-    );
+    // sendBasicEmail(
+    //   "airlineservice@gmail.com",
+    //   "airlineservice@gmail.com",
+    //   "This is a tetsing email",
+    //   "Hey, how are you?"
+    // );
+    // cron.schedule("*/4 * * * * *", () => {
+    //   console.log("running a task every two minutes");
+    // });
   });
 };
 
